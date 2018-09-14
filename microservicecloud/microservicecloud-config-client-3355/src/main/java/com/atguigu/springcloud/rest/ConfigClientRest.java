42f33785e950fbca57408d242fe47b3c17a57a6c
wukn2018 <wukn@xinguangnet.com>
2018-09-14 16:06:57 +0800

    springcloud2018/9/14

---
+++ microservicecloud/microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/rest/ConfigClientRest.java
@@ -0,0 +1,37 @@
+package com.atguigu.springcloud.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientRest
+{
+
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${eureka.client.service-url.defaultZone}")
+	private String eurekaServers;
+
+	@Value("${server.port}")
+	private String port;
+
+	@RequestMapping("/config")
+	public String getConfig()
+	{
+		String str = "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
+		System.out.println("******str: " + str);
+		return "applicationName: " + applicationName + "\t eurekaServers:" + eurekaServers + "\t port: " + port;
+	}
+
+
+
+	@Test
+	public void test01() {
+		System.out.println("ww");
+	}
+
+
+}
